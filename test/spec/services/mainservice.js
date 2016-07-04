ecb44c805e2b9f1c93e10bcba588ac80652adfb9
Jacques Uys <jaxuys@gmail.com>
2016-07-04 09:56:16 +0200

    Test for Repository listings

--- test/spec/services/mainservice.js
+++ test/spec/services/mainservice.js
@@ -7,12 +7,39 @@ describe('Service: mainService', function () {
 
   // instantiate service
   var mainService;
-  beforeEach(inject(function (_mainService_) {
+  var httpBackend;
+
+  beforeEach(inject(function (_mainService_, $httpBackend) {
     mainService = _mainService_;
+    httpBackend = $httpBackend;
   }));
 
   it('should do something', function () {
     expect(!!mainService).toBe(true);
   });
+
+  it('should list the repositories, per query', inject(function($http) {
+    var $scope = {};
+
+    /* Code Under Test */
+    $http.get('https://api.github.com/search/repositories?q=asxsaxsax')
+    .success(function(data, status, headers, config) {
+      $scope.valid = true;
+      $scope.response = data;
+    })
+    .error(function(data, status, headers, config) {
+      $scope.valid = false;
+    });
+    /* End */
+
+    httpBackend
+    .when('GET', 'https://api.github.com/search/repositories?q=asxsaxsax')
+    .respond(200, {"total_count": 0, "incomplete_results": false, "items": []} );
+
+    httpBackend.flush();
+
+    expect($scope.valid).toBe(true);
+    expect($scope.response).toEqual({"total_count": 0, "incomplete_results": false, "items": []});
+  }));
 
 });

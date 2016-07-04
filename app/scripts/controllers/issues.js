55ce8aff9ba2b32b1358ca58bf5e70030c61cade
Jacques Uys <jaxuys@gmail.com>
2016-07-04 23:17:24 +0200

    toggling for accordion

--- app/scripts/controllers/issues.js
+++ app/scripts/controllers/issues.js
@@ -13,6 +13,7 @@ angular.module('blackswanApp')
     $scope.repo = $routeParams.repo ? $routeParams.repo : 'error';
     $scope.user = $routeParams.user ? $routeParams.user : 'error';
     $scope.loading = true;
+    $scope.toggle = 0;
 
     if ($routeParams.user && $routeParams.repo) {
       $http.get('https://api.github.com/search/issues?q=repo:'+ $routeParams.user + '/' + $routeParams.repo)
@@ -21,4 +22,8 @@ angular.module('blackswanApp')
         $scope.issues = repsonse.data;
       });
     }
+
+    $scope.accordion = function(index) {
+      $scope.toggle = index;
+    };
   });

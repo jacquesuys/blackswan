d8a1a3ce3a83e066ea40c0a75349c02f60ec46ef
Jacques Uys <jaxuys@gmail.com>
2016-07-05 14:04:39 +0200

    directives for the accordion headings and panels

---
+++ app/scripts/directives/accordion.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name blackswanApp.directive:accordion
+ * @description
+ * # accordion
+ */
+angular.module('blackswanApp')
+  .directive('accordion', function () {
+    return {
+      templateUrl: 'views/repos.html'
+    };
+  })
+  .directive('panelHeading', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      template: '<div class="panel-heading">'+
+        '<h4 class="panel-title" ng-click="accordion($index)">'+
+          '<div ng-transclude></div>'+
+        '</h4>'+
+      '</div>'
+    };
+  })
+  .directive('panelCollapse', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      template: '<div class="panel-collapse collapse {{ $index === toggle ? \'in\' : \'\' }}">'+
+        '<div class="panel-body">'+
+          '<div ng-transclude></div>'+
+        '</div>'+
+      '</div>'
+    };
+  });;

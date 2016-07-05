e72392df34f0e8ab12a7fd5a33ecebe9fc32621d
Jacques Uys <jaxuys@gmail.com>
2016-07-05 14:11:25 +0200

    removed redundant tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -17,11 +17,4 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
-
-  it('test', function() {
-    expect(scope.test).toBe('test');
-  });
 });

15e4adaedbbcaa4a1a8e87093e9887cbfa870e38
Jacques Uys <jaxuys@gmail.com>
2016-07-06 15:06:26 +0200

    ChartJS init & promise catch

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,6 +23,42 @@ angular.module('blackswanApp')
         $scope.loading = false;
         $scope.repositories = response.data;
         $scope.message = 'Displaying ' + response.data.items.length + ' results for "' + repo + '"';
+        chart();
+      })
+      .catch(function(){
+        $scope.message = 'An error occurred';
+      });
+    }
+
+    function chart(index) {
+      index = index || 0;
+      var ctx = angular.element("#myChart");
+      var repo = $scope.repositories.items[index]
+      var myChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+          labels: ["Forks", "Starsgazers", "Open Issues"],
+          datasets: [{
+            label: 'Stats for ' + repo.full_name,
+            data: [repo.forks, repo.stargazers_count, repo.open_issues_count],
+            backgroundColor: [
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+              'rgba(75, 192, 192, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            xAxes: [{ ticks: { beginAtZero:true } }]
+          }
+        }
       });
     }
 
@@ -35,5 +71,6 @@ angular.module('blackswanApp')
 
     $scope.accordion = function(index) {
       $scope.toggle = index;
+      chart(index);
     };
   });
